c94eb1ceeb90a16d2d765cd10e88f8e37393f237
aleksandr.pavozkov <apavozkov@bk.ru>
2025-05-01 19:55:09 +0300

    Добавлены файлы генератора гентипов и генератора мутаций + файл с ограничениями для генотипа

---
+++ Gennadiy.clj
@@ -0,0 +1,22 @@
+(ns genotype-generator.core
+  (:require [clojure.edn :as edn]))
+
+(defn read-constraints [filename]
+  (try
+    (-> (slurp filename)
+        (edn/read-string))
+    (catch Exception e
+      (println "Ошибка чтения файла:" (.getMessage e))
+      (System/exit 1))))
+
+(defn generate-number [[min max]]
+  (+ min (rand-int (inc (- max min)))))
+
+(defn -main []
+  (let [constraints (read-constraints "limits.edn")]
+    (when (not= 16 (count constraints))
+      (println "Ошибка: требуется ровно 16 пар чисел")
+      (System/exit 1))
+    (println (vec (map generate-number constraints)))))
+
+(-main)
